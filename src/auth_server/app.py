c97aa44b37b9fd566cbfb10ab638449aa1d5f1b3
Vikram Modugu <moduguvikram@gmail.com>
2025-11-01 23:52:08 -0500

    swagget issue fix.

--- src/auth_server/app.py
+++ src/auth_server/app.py
@@ -58,7 +58,10 @@ API_URL = '/static/swagger.json'
 swaggerui_blueprint = get_swaggerui_blueprint(
     SWAGGER_URL,
     API_URL,
-    config={'app_name': "OAuth2 Server with OTP"}
+    config={
+        'app_name': "OAuth2 Server with OTP",
+        'tryItOutEnabled': True
+    }
 )
 app.register_blueprint(swaggerui_blueprint, url_prefix=SWAGGER_URL)
 
